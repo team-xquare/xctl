2afe7b91d50d141a3e2a119563ed866d2871b330
hwc9169 <hwc9169@gmail.com>
2022-05-16 23:33:51 +0900

    ⚡️ ::  create 명령 추가

--- cmd/create/create.go
+++ cmd/create/create.go
@@ -5,23 +5,35 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+
+	cmdutil "github.com/xctl/cmd/util"
 )
 
 // createCmd represents the create command
 func NewCmdCreate() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "create NAME",
-		Short: "Create and deploy app for XQUARE k8s manifest",
-		Long:  "Create and deploy app for XQUARE k8s manifest.",
+		Use:   "create",
+		Short: "Create a resource of xquare cluster",
+		Long:  "Create a resource of xquare cluster.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("create called")
+			cmdutil.RequireNoArguments(cmd, args)
+			cmd.Help()
 		},
 	}
 
 	cmd.AddCommand(NewCmdCreateApp())
 
 	return cmd
+}
+
+func NameFromCommandArgs(cmd *cobra.Command, args []string) (string, error) {
+	argsLen := cmd.ArgsLenAtDash()
+	if argsLen == -1 {
+		argsLen = len(args)
+	}
+	if argsLen != 1 {
+		return "", cmdutil.UsageErrorf(cmd, "exactly one NAME is required, got %d", argsLen)
+	}
+	return args[0], nil
 }
