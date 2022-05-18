eb896c594834a153b06db59c62d641f65c8e6f3c
hwc9169 <hwc9169@gmail.com>
2022-05-18 23:51:57 +0900

    ⚡️ :: config 모듈 추가

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	Token string
+}
+
+var Config config
+
+func MustLoadConfig() {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatalln("Fatal not found config file:", err)
+		} else {
+			log.Fatalln("Fatal error config file:", err)
+		}
+	}
+
+	viper.Unmarshal(&Config)
+}
