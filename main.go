e182eba0e2b7a3557b753bdf5eeaaf3c293f5bb9
hwc9169 <hwc9169@gmail.com>
2022-05-18 23:51:35 +0900

    ⚡️ :: 설정 로딩 추가

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/xctl/cmd"
+import (
+	"github.com/xctl/cmd"
+	"github.com/xctl/config"
+)
+
+func init() {
+	config.MustLoadConfig()
+}
 
 func main() {
 	cmd.Execute()
