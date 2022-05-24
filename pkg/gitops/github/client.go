09988e7452521969159b543a59f38598139f91fe
hwc9169 <hwc9169@gmail.com>
2022-05-24 17:17:24 +0900

    ♻️ :: gitops 클라이언트

---
+++ pkg/gitops/github/client.go
@@ -0,0 +1,85 @@
+package github
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/go-github/github"
+	"github.com/xctl/config"
+	"golang.org/x/oauth2"
+)
+
+var (
+	DefaultRepoOwner      = "team-xquare"
+	DefaultCommitterName  = "xquare-admin"
+	DefaultCommitterEmail = "teamxquare@gmail.com"
+
+	StagingRepo    = "xquare-gitops-repo-staging"
+	ProductionRepo = "xquare-gitops-repo-production"
+
+	FileType = github.String("blob")
+	ReadMode = github.String("100644")
+)
+
+type GithubClient struct {
+	Client         *github.Client
+	Repo           string
+	RepoOwner      string
+	CommitterName  string
+	CommitterEmail string
+}
+
+func NewGithubClient(repo string) *GithubClient {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: config.Config.GithubToken})
+	tc := oauth2.NewClient(context.Background(), ts)
+	client := github.NewClient(tc)
+
+	return &GithubClient{
+		Client:         client,
+		Repo:           repo,
+		RepoOwner:      DefaultRepoOwner,
+		CommitterName:  DefaultCommitterName,
+		CommitterEmail: DefaultCommitterEmail,
+	}
+}
+
+func (g *GithubClient) GetRef(ctx context.Context) (ref *github.Reference, err error) {
+	ref, _, err = g.Client.Git.GetRef(ctx, g.RepoOwner, g.Repo, "refs/heads/master")
+	if err != nil {
+		return ref, err
+	}
+
+	return ref, nil
+}
+
+func (g *GithubClient) GetHeadFromRef(ctx context.Context, ref *github.Reference) (head *github.RepositoryCommit, err error) {
+	head, _, err = g.Client.Repositories.GetCommit(ctx, g.RepoOwner, g.Repo, *ref.Object.SHA)
+	if err != nil {
+		return nil, err
+	}
+	head.Commit.SHA = head.SHA
+
+	return head, err
+}
+
+func (g *GithubClient) CreateCommit(ctx context.Context, message string, tree *github.Tree, head *github.Commit) (newCommit *github.Commit, err error) {
+	date := time.Now()
+	author := &github.CommitAuthor{Date: &date, Name: &g.CommitterName, Email: &g.CommitterEmail}
+	commit := &github.Commit{Author: author, Message: &message, Tree: tree, Parents: []github.Commit{*head}}
+	newCommit, _, err = g.Client.Git.CreateCommit(ctx, g.RepoOwner, g.Repo, commit)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+func (g *GithubClient) UpdateRef(ctx context.Context, ref *github.Reference, newCommit *github.Commit) (err error) {
+	ref.Object.SHA = newCommit.SHA
+	_, _, err = g.Client.Git.UpdateRef(ctx, g.RepoOwner, g.Repo, ref, false)
+	return
+}
+
+func (g *GithubClient) CreateTreeFromEntries(ctx context.Context, ref *github.Reference, entries []github.TreeEntry) (tree *github.Tree, err error) {
+	tree, _, err = g.Client.Git.CreateTree(ctx, g.RepoOwner, g.Repo, *ref.Object.SHA, entries)
+	return tree, err
+}
