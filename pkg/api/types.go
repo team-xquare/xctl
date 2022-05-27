d48c936bfb034095df9f941d299b7ae5995ed7e1
hwc9169 <hwc9169@gmail.com>
2022-05-27 14:54:14 +0900

    ⚡️ :: 서비스 type, environment 체크 기능 추가

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -1,20 +1,48 @@
 package api
 
+import "fmt"
+
+type Application struct {
+	Name          string
+	Host          string
+	ImageRegistry string
+	ImageTag      string
+	ContainerPort int32
+	Prefix        string
+	Environment   string
+	Type          string
+}
+
 const (
 	Backend  = "backend"
 	Frontend = "frontend"
+)
 
+func CheckApplicationType(t string) (string, error) {
+	switch t {
+	case "backend", "be":
+		return Backend, nil
+	case "frontend", "fe":
+		return Frontend, nil
+	default:
+		return "", fmt.Errorf("cannot use an application type: %s.\n support \"frontend\", \"fe\", \"backend\" \"be\" only", t)
+
+	}
+}
+
+const (
 	Production = "production"
 	Staging    = "staging"
 )
 
-type Application struct {
-	Name          string
-	Type          string
-	Host          string
-	ImageRegistry string
-	ImageTag      string
-	Environment   string
-	ContainerPort int32
-	Prefix        string
+func CheckApplicationEnvironment(e string) (string, error) {
+	switch e {
+	case "production", "prod":
+		return Production, nil
+	case "staging", "stag":
+		return Staging, nil
+	default:
+		return "", fmt.Errorf("cannot use an application environment: %s.\n support \"production\", \"prod\", \"staging\" \"stag\" only", e)
+
+	}
 }
